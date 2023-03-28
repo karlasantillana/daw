14612fc7630f063b29a1c75c9a5264b60e7f615a
karlasantillana <karlasantillanaayulo@gmail.com>
2023-03-29 01:26:52 +0200

    array

--- curso_php/arrays.php
+++ curso_php/arrays.php
@@ -11,23 +11,45 @@
     
         
         //ARRAY INDEXADO
-        /*
+        
         $semana[]="Lunes";
 
         $semana[]="Martes";
 
         $semana[]="Miércoles";
-        */
+
+        $semana[]="Jueves";
+
+        //para recorrer array indexado con bucle for, del cual no sé la cantidad de elementos
+        for($i=0 ; $i<count($semana) ; $i++){
+
+            echo $semana[$i] . "<br>";
+        }
+        
 
 
-        //ARRAY ASOCIATIVO
+        //funcion array()
         /*
         $semana = array("Lunes" , "Martes" , "Miércoles" , "Jueves");
 
         echo $semana[3];
         */
 
-        $datos=array();
+        //ARRAY ASOCIATIVO
+        //ej datos personales de una persona
+        /*
+        $datos=array("Nombre"=>"Jona" , "Apellido"=>"Paredes" , "Edad"=>36);
+
+        //$datos="Martín";
+
+        //echo $datos;
+
+        foreach($datos as $clave=>$valor){
+
+            echo "A $clave le corresponde $valor <br>";
+        }
+        */
+
     ?>
 </body>
 </html>
