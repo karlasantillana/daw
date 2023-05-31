6d6b85cbf6d97efb10aa5f9cf97b25150e77c42e
karlasantillana <karlasantillanaayulo@gmail.com>
2023-05-31 08:52:52 +0200

    conexiones

--- curso_php/conexionPDO.php
+++ curso_php/conexionPDO.php
@@ -14,7 +14,7 @@
     try{
 
         $base = new PDO ("mysql:host = localhost ; dbname=pruebas" , "root" , ""); 
-        
+        $base->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
 
         echo "Conexión OK";
 
