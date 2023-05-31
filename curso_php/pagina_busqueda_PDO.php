5061272d4d6844e35a712a43fae43b2f3c3f3fb7
karlasantillana <karlasantillanaayulo@gmail.com>
2023-05-31 08:51:10 +0200

    conexiones

--- curso_php/pagina_busqueda_PDO.php
+++ curso_php/pagina_busqueda_PDO.php
@@ -1,20 +1,9 @@
 <?php
-
-/*
-    //creo variable, instancio clase PDO
-            ARGUMENTOS: 
-            //1er arg-->host con el que quiero conectar y nombre de la bbdd
-            //2do arg-->nombre de usuario
-            //3er arg-->password
-
-    //Conexión satisfactoria
-            
-*/
-
     try{
 
         $base = new PDO ("mysql:host = localhost ; dbname=pruebas" , "root" , ""); 
         
+        $base->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
 
         echo "Conexión OK";
 
@@ -29,6 +18,5 @@
         echo "<br>Vaciando la memoria para ahorrar recursos";
         $base = null; //vaciar la memoria
     }
-    //en este caso, si entra al catch, no se llega a realizar el finally ya que llega a entrar al die()
-    
+
 ?>
