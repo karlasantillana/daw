c1ed3408c6baeb51e6aeeb6c8d3541caba4b197b
karlasantillana <karlasantillanaayulo@gmail.com>
2023-06-05 20:33:47 +0200

    importar tablas

--- curso_php/conexion_BBDD.php
+++ curso_php/conexion_BBDD.php
@@ -1,50 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>conexionBBDD</title>
+
+    <style>
+        table{
+            width: 50%;
+            border: 1px dotted red;
+            margin: auto;
+        }
+    </style>
+</head>
+
+<body>
 <?php
-    require("dato_conexion.php");
+    /******************************conexión con Mysqli***************/
+    require("datos_conexion.php");
 
     //CONECTAR CON BBDD
     $conexion = mysqli_connect($db_host , $db_usuario , $db_contraseña , $db_nombre);
 
 
     //Consultas o query o sentencias sql
-    $consulta="SELECT * FROM DATOSPERSONALES";
+    $consulta="SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN ='ESPAÑA'";
 
     //Ejecutar la consulta con la función mysqli_query($conexion , $sql); con dos argumentos
     $resultados = mysqli_query($conexion , $consulta);
-
-/*
-    //obtener registros con bucle WHILE (sabiendo la cantidad de registros que hay)
-    $registros=1;
-
-    while(($fila = mysqli_fetch_row($resultados))==true){
-
-        $fila = mysqli_fetch_row($resultados);
-
-        echo $fila[0] ." ";
-        echo $fila[1] ." ";
-        echo $fila[2] ." ";
-        echo $fila[3] ." ";
-    
-        echo "<br>";
-    
-        $registros++;
-    }
-*/
-
 
 
     //obtener registros con bucle WHILE (*NO* sabiendo la cantidad de registros que hay) 
     //Prescindo de la variable $registros y del condicional booleano TRUE
     while($fila = mysqli_fetch_row($resultados)){
 
-        echo $fila[0] ." ";
-        echo $fila[1] ." ";
-        echo $fila[2] ." ";
-        echo $fila[3] ." ";
-    
+        echo "<table><tr>";
+        echo "<td>". $fila[0] ."</td>";
+        echo "<td>". $fila[1] ."</td>";
+        echo "<td>". $fila[2] ."</td>";
+        echo "<td>". $fila[3] ."</td>";
+        echo "<td>". $fila[4] ."</td>";
+        echo "<td>". $fila[5] ."</td>";
+        echo "<td>". $fila[6] ."</td></tr></table>";
+
+        echo "<br>";
         echo "<br>";
     }
 
     //CERRAR CONEXIÓN
     mysqli_close($conexion);
-
 ?>
+</body>
+</html>
+
